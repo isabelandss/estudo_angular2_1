da9e34f2e3e4c46b52aa558d646bd2eecde3a353
Isabela Neri dos Santos <isabelandss@gmail.com>
2017-09-14 10:52:37 -0300

    Aula 1-11

---
+++ client/app/app.component.ts
@@ -0,0 +1,10 @@
+//decorator: indicando que essa classe é um componente no sistema
+//dentro de core tem o Component
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './app/app.component.html'
+})
+
+export class AppComponent { }
